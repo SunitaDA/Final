e24c8af6f01e72c6de9ae1cfebb84a0160cb95bf
SunitaDA <144669837+SunitaDA@users.noreply.github.com>
2023-09-19 16:52:22 +0530

    values insertion.

--- source/V1.1.2__sales2.sql
+++ source/V1.1.2__sales2.sql
@@ -2,4 +2,4 @@ INSERT INTO Sales (SaleID, ProductName, SaleDate, SaleAmount)
 VALUES
     (2, 'Product B', '2023-09-20', 150.00),
     (3, 'Product A', '2023-09-21', 75.50),
-    (4, 'Product C', '2023-09-22', 200.35);
+    (4, 'Product C', '2023-09-22', 200.25);
